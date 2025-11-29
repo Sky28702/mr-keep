dd3cb6e5694a3dbb5966893e5e2e638b99a32131
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-11-30 02:11:21 +0530

    home basic design done

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,9 +1,19 @@
+import { IconPlus } from "@tabler/icons-react";
+
 function Home() {
   return (
-    <>
-      <div>yo</div>
-      // emerald 400
-    </>
+    <section className=" relative flex justify-center items-center flex-col h-screen">
+      {/* <img
+        src="/write.png"
+        className=" h-auto mb-2 max-w-full min-w-[70%] "
+      ></img>
+      <p className="font-normal text-[22px]">Start Writing Your Notes</p> */}
+      <div className=" fixed bg-emerald-400 cursor-pointer bottom-6 right-6 rounded-full">
+        <span className="font-2xl text-white">
+          <IconPlus stroke={2} size={50} />
+        </span>
+      </div>
+    </section>
   );
 }
 
