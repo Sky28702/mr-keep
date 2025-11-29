737691f393e842ff66b3314faca2e7b922ffe5f2
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-11-30 05:06:30 +0530

    login front end DONEEE

--- app/(authentication)/sign-in/page.js
+++ app/(authentication)/sign-in/page.js
@@ -1,24 +1,66 @@
+"use client";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
 function login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  async function atSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <section className="p-1">
       <h1 className="font-bold text-3xl mt-2">Login</h1>
-      <form>
+      <form onSubmit={handleSubmit(atSubmit)}>
         <div className="flex flex-col md:flex-row justify-between items-center md:mt-40 p-1">
           <img
             src="/logIn.png"
             className=" h-70 w-90 mb-2  md:mt-0  mt-20"
           ></img>
           <div className="flex flex-col justify-center items-center gap-4  w-full md:w-100 ">
-            <input
-              placeholder="User Name"
-              className=" min-w-full  active:outline-emerald-400 active:outline-2 outline-none focus:outline  border  border-emerald-400  p-2 rounded-2xl focus:outline-emerald-400"
-            ></input>
+            <div className="w-full">
+              <input
+                placeholder="User Name"
+                className=" min-w-full  active:outline-emerald-400 active:outline-2 outline-none focus:outline  border  border-emerald-400  p-2 rounded-2xl focus:outline-emerald-400"
+                {...register("userName", {
+                  required: "Username is required",
+                  minLength: {
+                    value: 3,
+                    message: "Username must be at least 3 characters",
+                  },
+                })}
+              ></input>
+              {errors.userName && (
+                <p className="text-red-600 mb-2 mt-2">
+                  {errors.userName.message}
+                </p>
+              )}
+            </div>
 
-            <input
-              placeholder="Password"
-              className=" mb-4 min-w-full active:outline-emerald-400 active:outline-2 outline-none focus:outline border  border-emerald-400  p-2 rounded-2xl  focus:outline-emerald-400"
-            ></input>
+            <div className="w-full">
+              <input
+                placeholder="Password"
+                className=" mb-4 min-w-full active:outline-emerald-400 active:outline-2 outline-none focus:outline border  border-emerald-400  p-2 rounded-2xl  focus:outline-emerald-400"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+              ></input>
+              {errors.password && (
+                <p className="text-red-600 mb-2 mt-2">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
             <p className="font-normal mb-4">
               New Here?{" "}
               <Link href="/sign-up" className="text-emerald-400">
