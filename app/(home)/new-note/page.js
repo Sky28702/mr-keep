7f43fa6a3dbeae5909218ae170413315c7497983
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-12-06 23:56:03 +0530

    create done...

--- app/(home)/new-note/page.js
+++ app/(home)/new-note/page.js
@@ -1,32 +1,90 @@
 "use client";
 import { IconDeviceFloppy } from "@tabler/icons-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IconLoader2 } from "@tabler/icons-react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import axios, { Axios } from "axios";
 
 export default function CreateNote() {
-  const [height, setHeight] = useState(50);
+  const router = useRouter();
+  useEffect(() => {
+    const localData = localStorage.getItem("Current User");
+    if (localData) {
+      const user = JSON.parse(localData);
+    } else {
+      router.push("/sign-up");
 
-  // if(sizeof)
+      return;
+    }
+
+    // async function statusOfLike() {
+    //   try {
+    //     setLoading(true);
+    //     await statusLike(data, setIsClick);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // }
+
+    // statusOfLike();
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [loading, setLoading] = useState(false);
+
+  async function atSubmit(data) {
+    try {
+      try {
+        setLoading(true);
+        const res = await axios.post("/backend/api/notes", data);
+        console.log(res.data);
+      } finally {
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <section className="p-4 relative">
       <h1 className="text-3xl font-semibold text-emerald-400 mb-4">
         Create New Note
       </h1>
-      <form className=" flex flex-col item-center   ">
+      <form
+        onSubmit={handleSubmit(atSubmit)}
+        className=" flex flex-col item-center   "
+      >
         <textarea
           placeholder="Title"
           className=" font-medium field-sizing-content text-3xl min-h-[50px] max-h-[300px] w-full resize-none mb-6 outline-none overflow-hidden"
           maxLength={20}
+          {...register("title")}
         ></textarea>
 
         <textarea
           placeholder="Take a note..."
           className="w-auto text-[20px] field-sizing-content h-screen outline-none"
-          maxLength={200}
+          {...register("text")}
         ></textarea>
 
-        <button className="bg-emerald-400 text-white p-4 cursor-pointer text-[20px] rounded-full fixed bottom-6 right-6">
-          <IconDeviceFloppy stroke={2} />
+        <button
+          disabled={loading}
+          className="bg-emerald-400 text-white p-4 cursor-pointer text-[20px] rounded-full fixed bottom-6 right-6"
+        >
+          {loading == true ? (
+            <span className="animate-spin">
+              <IconLoader2 stroke={2} />
+            </span>
+          ) : (
+            <IconDeviceFloppy stroke={2} />
+          )}
         </button>
       </form>
     </section>
--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,54 +1,88 @@
+"use client";
 import { IconPlus, IconTrash, IconEye, IconEdit } from "@tabler/icons-react";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [notes, setNotesData] = useState(null);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("Current User");
+    const user = JSON.parse(localData);
+    if (localData) {
+      setLoggedIn(true);
+    } else {
+      return;
+    }
+
+    async function readAllNotes() {
+      try {
+        try {
+          setLoading(true);
+          const res = await axios.get("/backend/api/notes", user.id);
+          console.log(res.data);
+        } finally {
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    readAllNotes();
+  }, []);
+
   return (
     <section className=" relative p-2">
       <h1 className="font-bold text-3xl align-left mt-2 mb-10">Mr-Keep</h1>
-
-      <div className=" relative py-2 pr-4 pl-2 border border-none shadow shadow-emerald-400 bg-emerald-50 hover:bg-emerald-100 cursor-pointer transition-shadow rounded-[10px] w-full align-left h-40">
-        <p className="truncate mb-1 font-semibold text-2xl">LODA LESUN </p>
-        <p className="line-clamp-3 overflow-hidden text-gray-800">
-          jbfsdbsalfdslknsdlk dfoidsiodfsodisiof difjndsandfpsdsnf dpojdfpodfs
-          dosofjdspjdf dspodnspofsnd podofpdojdopsf podfjspodjfs
-          jbfsdbsalfdslknsdlk dfoidsiodfsodisiof difjndsandfpsdsnf dpojdfpodfs
-          dosofjdspjdf dspodnspofsnd podofpdojdopsf podfjspodjfs
-          jbfsdbsalfdslknsdldifjndsandfpsdsnf dpojdfpodfs dosofjdspjdf
-          dspodnspofsnd podofpdojdopsf podfjspodjfs jbfsdbsalfdslknsdlk
-          dfoidsiodfsodisiof difjndsandfpsdsnf dpojdfpodfs dosofjdspjdf
-          dspodnspofsnd podofpdojdopsf podfjspodjfs jbfsdbsalfdslknsdlk
-          dfoidsiodfsodisiof difjndsandfpsdsnf dpojdfpod
-        </p>
-
-        <div className="absolute bottom-1 right-2 flex flex-row gap-2 items-center">
-          <p className="text-gray-700  font-light text-[12px] ">
-            Last time editied : 12:00Am, 12-20-2025
-          </p>
-
-          <p title="delete" className="cursor-pointer text-red-500 text-[18px]">
-            <IconTrash stroke={2} />
-          </p>
-          <p title="view" className="cursor-pointer text-[18px]">
-            <IconEye stroke={2} />
-          </p>
-          <p
-            title="Edit"
-            className="cursor-pointer text-emerald-400 text-[18px] "
-          >
-            <IconEdit stroke={2} />
-          </p>
+      {loading == true || loggedIn == false ? (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+          <img
+            src="/write.png"
+            className="h-auto mb-2 max-w-full min-w-[70%]"
+          />
+          <p className="font-normal text-[22px]">Start Writing Your Notes</p>
         </div>
-      </div>
+      ) : (
+        <div className=" relative py-2 pr-4 pl-2 border border-none shadow shadow-emerald-400 bg-emerald-50 hover:bg-emerald-100 cursor-pointer transition-shadow rounded-[10px] w-full align-left h-40">
+          <p className="truncate mb-1 font-semibold text-2xl">LODA LESUN </p>
+          <p className="line-clamp-3 overflow-hidden text-gray-800">
+            //sample text
+          </p>
 
-      {/* <div className="flex flex-col items-center justify-center min-h-screen text-center">
-        <img src="/write.png" className="h-auto mb-2 max-w-full min-w-[70%]" />
-        <p className="font-normal text-[22px]">Start Writing Your Notes</p>
-      </div> */}
+          <div className="absolute bottom-1 right-2 flex flex-row gap-2 items-center">
+            <p className="text-gray-700  font-light text-[12px] ">
+              Last time editied : 12:00Am, 12-20-2025
+            </p>
 
-      <div className=" fixed bg-emerald-400 cursor-pointer bottom-6 right-6 rounded-full">
-        <span className="font-2xl text-white">
-          <IconPlus stroke={2} size={50} />
-        </span>
-      </div>
+            <p
+              title="delete"
+              className="cursor-pointer text-red-500 text-[18px]"
+            >
+              <IconTrash stroke={2} />
+            </p>
+            <p title="view" className="cursor-pointer text-[18px]">
+              <IconEye stroke={2} />
+            </p>
+            <p
+              title="Edit"
+              className="cursor-pointer text-emerald-400 text-[18px] "
+            >
+              <IconEdit stroke={2} />
+            </p>
+          </div>
+        </div>
+      )}
+
+      <Link href={`/new-note`}>
+        <div className=" fixed bg-emerald-400 cursor-pointer bottom-6 right-6 rounded-full">
+          <span className="font-2xl text-white">
+            <IconPlus stroke={2} size={50} />
+          </span>
+        </div>
+      </Link>
     </section>
   );
 }
