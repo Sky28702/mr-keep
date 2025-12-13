a81a49549e1e2133d61d4881714cd9e7b6518106
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-12-13 23:27:05 +0530

    tmro

--- app/(home)/new-note/page.js
+++ app/(home)/new-note/page.js
@@ -23,7 +23,7 @@ export default function CreateNote() {
     //   try {
     //     setLoading(true);
     //     await statusLike(data, setIssClick);
-    //   } finally { ss
+    //   } finally { ssss
     //     setLoading(false);
     //   }
     // }
